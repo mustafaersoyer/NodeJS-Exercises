81657399e5fa88e41fd50e76fa60cd203cc78f11
Mustafa Ersoy <1mustafaersoy@gmail.com>
2022-04-12 21:27:37 +0300

    Update hackerRankEncryption.ts

--- hackerRankEncryption.ts
+++ hackerRankEncryption.ts
@@ -26,13 +26,6 @@ function readLine(): string {
   return inputLines[currentLine++];
 }
 
-/*
- * Complete the 'encryption' function below.
- *
- * The function is expected to return a STRING.
- * The function accepts STRING s as parameter.
- */
-
 function encryption(s: string): string {
   // Write your code here
   let spaceRemoved: string = s.replace(" ", "");
